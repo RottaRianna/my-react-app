372163422fd7c1bd1d7cf4f7f3ee17f4e9892564
RottaRianna <151004383+RottaRianna@users.noreply.github.com>
2026-04-21 16:33:59 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
-import { createRoot } from 'react-dom/client'
 
+import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>
+)
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import './style.css';
